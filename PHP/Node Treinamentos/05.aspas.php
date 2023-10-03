de193287e7fb0a6de71aa92f01c123c7d07d5450
Peterson Gabriel dos Santos <PetersonGabriel3003@gmail.com>
2023-10-02 23:33:29 -0300

    atualizando

--- PHP/Node Treinamentos/05.aspas.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Aspas e concatenação
-    // Aspas simples são literais (texto)
-    // Aspas duplas são interpretativas (podemos usar as variaveis)
-    $nome = 'Peterson Gabriel';
-    // USamos aspas duplas ou concatenamos a variavel
-    echo "Meu nome é \"$nome\" <br>";
-    echo "<hr>";
-    echo 'Meu nome é '.$nome.' e minha idade é \'20\'';
